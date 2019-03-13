8c52322ca4c3c91b0ab3582ac2d4cb3680711cbb
arushpareek <40450269+arushpareek@users.noreply.github.com>
2019-03-13 08:36:18 +0530

    this is wrong message

--- test.py
+++ test.py
@@ -4,3 +4,5 @@ print "I have added new line *****"
 print "this line added on local repo"
 print "one more line added"
 print "line added in branch2"
+
+print "line added to check ammend to commit message"
